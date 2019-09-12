182933a84d10f4fe647220b1e38e0a33f928e593
soucer10 <hugobicudo0@gmail.com>
2019-09-12 06:38:47 -0300

    Ajuste nas rotas

--- src/routes.js
+++ src/routes.js
@@ -7,9 +7,8 @@ routes.post('/cadastrar',UsersController.cadastrar)
 routes.post('/atualizar',UsersController.atualizar)
 routes.post('/deletar',UsersController.deletar)
 routes.post('/login',UsersController.login)
-routes.get("/",(req,res)=>{
-    return res.send("A minha magrelinha é linda")
+routes.get("/:name",(req,res)=>{
+    return res.send(`<h1>Olá ${req.params.name}, com você está?</h1>`);
+
 })
-
-
 module.exports=routes
